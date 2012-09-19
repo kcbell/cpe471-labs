0a9e9adcc4ca3a48b3b18138f139a0e78ee79452
Mark Henry <mhenry02@calpoly.edu>
2012-09-18 17:49:40 -0700

    Import

---
+++ main.cpp
@@ -0,0 +1,62 @@
+/*
+   CPE 471 Lab 1
+   Base code for Rasterizer
+   Example code using B. Somers' image code - writes out a sample tga
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Image.h"
+#include "types.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+   int IMG_WIDTH = 640;
+   int IMG_HEIGHT = 480;
+   
+   // Get input, one vertex per line
+   string input;
+   vertex_t vertices[3];
+   int min_x = IMG_WIDTH;
+   int max_x = 0;
+   int min_y = IMG_HEIGHT;
+   int max_y = 0;
+   for (int coord = 0; coord < 6; coord++)
+   {
+      if (coord % 2 == 0)
+      {
+         cin >> vertices[coord/2].x;
+         min_x = min(min_x, vertices[coord/2].x);
+         max_x = max(max_x, vertices[coord/2].x);
+      }
+      else
+      {
+         cin >> vertices[coord/2].y;
+         min_y = min(min_y, vertices[coord/2].y);
+         max_y = max(max_y, vertices[coord/2].y);
+      }
+   }
+   
+   // make a blue
+   color_t blue;
+   blue.r = 0.1;
+   blue.g = 0.1;
+   blue.b = 0.9;
+
+   // make a 640x480 image (allocates buffer on the heap)
+   Image img(640, 480);
+
+   //Render bounding box as blue
+   for (int y = min_y; y <= max_y; y++)
+      for (int x = min_x; x <= max_x; x++)
+      {
+         img.pixel(x, y, blue);
+      }
+
+   // write the targa file to disk
+   img.WriteTga((char *)"awesome.tga", true);
+   // true to scale to max color, false to clamp to 1.0
+}
---
+++ types.h
@@ -0,0 +1,18 @@
+#ifndef __TYPES_H__
+#define __TYPES_H__
+
+/* Color struct */
+typedef struct color_struct {
+   double r;
+   double g;
+   double b;
+   double f; // "filter" or "alpha"
+} color_t;
+
+typedef struct vertex_struct {
+   int x;
+   int y;
+   int z;
+} vertex_t;
+
+#endif
