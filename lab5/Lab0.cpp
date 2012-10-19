db9a5a077f4a0491bde6f798d123a681d1d5b83a
Mark Henry <theratcheteer+github@gmail.com>
2012-10-18 20:38:07 -0700

    SHIP IT

--- lab5/Lab0.cpp
+++ lab5/Lab0.cpp
@@ -43,21 +43,14 @@ GLint h_uProjMatrix;
 GLuint CubeBuffObj, CIndxBuffObj, GrndBuffObj, GIndxBuffObj, NormalBuffObj;
 int g_CiboLen, g_GiboLen;
 
-/* globals to control positioning and window size */
 static float g_width, g_height;
-float g_tx = -2.0;
-float g_ty = 0;
-float g_tz = 0;
-float g_trans = -5.5;
-float g_angle = 0;
-
 static const float g_groundY = 0.0;      // y coordinate of the ground
 static const float g_groundSize = 10.0;   // half the ground length
 
-int houseSizeX[] = { rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand() };
-int houseSizeZ[] = { rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand(), rand() };
+vec3 cube_translate;
+mat4 cube_rotate;
+float cube_scale;
 
-//declare a matrix stack
 RenderingHelper ModelTrans;
 
 /* projection matrix - do not change - sets matrix in shader */
@@ -78,31 +71,9 @@ void SetModel() {
    safe_glUniformMatrix4fv(h_uModelMatrix, glm::value_ptr(ModelTrans.modelViewMatrix));
 }
 
-/* intialize ground data */
-static void initGround() {
-
-   // A x-z plane at y = g_groundY of dimension [-g_groundSize, g_groundSize]^2
-   float GrndPos[] = { 
-      -g_groundSize, g_groundY, -g_groundSize, 
-      -g_groundSize, g_groundY,  g_groundSize, 
-      g_groundSize, g_groundY,  g_groundSize, 
-      g_groundSize, g_groundY, -g_groundSize
-   };
-   unsigned short idx[] = {0, 1, 2, 0, 2, 3};
-
-   g_GiboLen = 6;
-   glGenBuffers(1, &GrndBuffObj);
-   glBindBuffer(GL_ARRAY_BUFFER, GrndBuffObj);
-   glBufferData(GL_ARRAY_BUFFER, sizeof(GrndPos), GrndPos, GL_STATIC_DRAW);
-
-   glGenBuffers(1, &GIndxBuffObj);
-   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, GIndxBuffObj);
-   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(idx), idx, GL_STATIC_DRAW);
-}
-
 /* intialize the cube data */
-static void initCube() {
-
+static void initCube()
+{
    float CubePos[] = {
       -0.5, -0.5, -0.5, /*back face 5 verts :0 */
       -0.5, 0.5, -0.5,
@@ -135,15 +106,12 @@ static void initCube() {
    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, CIndxBuffObj);
    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(idx), idx, GL_STATIC_DRAW);
 
-   /* normal data - note it is empty right now 
-   glGenBuffers(1, &NormalBuffObj);
-   glBindBuffer(GL_ARRAY_BUFFER, NormalBuffObj);
-   glBufferData(GL_ARRAY_BUFFER, sizeof(CubeNormal), CubeNormal, GL_STATIC_DRAW);
-   */
+   cube_translate = vec3(0.0);
+   cube_scale = 0.0;
+   cube_rotate = mat4(1.0); //The identity matrix
 }
 
 void InitGeom() {
-   initGround();
    initCube();
 }
 
@@ -203,7 +171,6 @@ int InstallShader(const GLchar *vShaderName, const GLchar *fShaderName) {
    return 1;
 }
 
-
 /* Some OpenGL initialization */
 void Initialize ()					// Any GL Init Code 
 {
@@ -217,7 +184,6 @@ void Initialize ()					// Any GL Init Code
    /* some matrix stack init */
    ModelTrans.useModelViewMatrix();
    ModelTrans.loadIdentity();
-
 }
 
 /* Main display function */
@@ -231,29 +197,6 @@ void Draw (void)
    SetProjectionMatrix();
    SetView();
 
-
-   /******************* set up to draw the ground plane */
-   /*
-   safe_glEnableVertexAttribArray(h_aPosition);
-   // bind vbo
-   glBindBuffer(GL_ARRAY_BUFFER, GrndBuffObj);
-   safe_glVertexAttribPointer(h_aPosition, 3, GL_FLOAT, GL_FALSE, 0, 0);
-   // bind ibo
-   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, GIndxBuffObj);
-
-   // set the color in the shader 
-   glUniform3f(h_uColor, 0.7, 0.98, 0.9);
-
-   // set the matrix stack to the identity - no transforms on the ground
-   ModelTrans.loadIdentity();
-   SetModel();
-   // draw!
-   glDrawElements(GL_TRIANGLES, g_GiboLen, GL_UNSIGNED_SHORT, 0);
-
-   // Disable the attributes used by our shader
-   safe_glDisableVertexAttribArray(h_aPosition);
-   */
-
    /*********************** set up to draw the cube */
    safe_glEnableVertexAttribArray(h_aPosition);
    // bind vbo
@@ -262,67 +205,18 @@ void Draw (void)
    // bind ibo
    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, CIndxBuffObj);
 
+   glUniform3f(h_uColor, 0.5, 0.05, 0.2); //Cube color
+
    ModelTrans.pushMatrix();
 
-   // draw: set the color in the shader
-   glUniform3f(h_uColor, abs(sin((y+x) * 3.14159 / 8.0)), 0.78, abs(cos((y+x) * 3.14159 / 16.0)));
-   glDrawElements(GL_TRIANGLES, g_CiboLen - 3*6, GL_UNSIGNED_SHORT, 0);
-   glUniform3f(h_uColor, 0.3, 0.3, 0.3);
-   glDrawElements(GL_TRIANGLES, 3*6, GL_UNSIGNED_SHORT,
-      (GLvoid*)(sizeof(short)*(g_CiboLen - 3*6)));
+   ModelTrans.translate(cube_translate);
+   ModelTrans.scale(cube_scale, cube_scale, cube_scale);
+   ModelTrans.multMatrix(cube_rotate);
+
+   glDrawElements(GL_TRIANGLES, g_CiboLen, GL_UNSIGNED_SHORT, 0);
 
    ModelTrans.popMatrix();
    
-   /*
-   for (int x = 0; x < 2; x++)
-   {
-      spacing_sum = 0;
-
-      for (int y = 0; y < 10; y++)
-      {
-         ModelTrans.pushMatrix();
-
-         if (x == 0)
-         {
-            // Row across scene
-            float sizeX = ((houseSizeX[y] % 1000) / 500.0) + .2;
-            float sizeY = ((houseSizeX[y] % 2000) / 750.0) + .2;
-            spacing_sum += sizeX / 2.0;
-            ModelTrans.translate(vec3(g_tx + spacing_sum + xBlockXOffset, g_ty - 0.5, g_tz));
-            ModelTrans.scale(sizeX, sizeY, 1.0);
-            ModelTrans.translate(vec3(0, .5, 0));
-            spacing_sum += sizeX / 2.0;
-         }
-         else if (x == 1)
-         {
-            // Row coming towards camera
-            float sizeX = ((houseSizeZ[y] % 1000) / 500.0) + .2;
-            float sizeY = ((houseSizeZ[y] % 2000) / 750.0) + .2;
-            spacing_sum += sizeX / 2.0;
-            ModelTrans.translate(vec3(g_tx, g_ty - 0.5, g_tz + spacing_sum + zBlockZOffset));
-            ModelTrans.rotate(90, vec3(0, 1, 0));
-            ModelTrans.scale(sizeX, sizeY, 1.0);
-            ModelTrans.translate(vec3(0, .5, 0));
-            spacing_sum += sizeX / 2.0;
-         }
-
-         // set up where to draw the box 
-         SetModel();
-         // draw!
-         // set the color in the shader
-         glUniform3f(h_uColor, abs(sin((y+x) * 3.14159 / 8.0)), 0.78, abs(cos((y+x) * 3.14159 / 16.0)));
-         glDrawElements(GL_TRIANGLES, g_CiboLen - 3*6, GL_UNSIGNED_SHORT, 0);
-         glUniform3f(h_uColor, 0.3, 0.3, 0.3);
-         glDrawElements(GL_TRIANGLES, 3*6, GL_UNSIGNED_SHORT,
-            (GLvoid*)(sizeof(short)*(g_CiboLen - 3*6)));
-
-         ModelTrans.popMatrix();
-      }
-   }
-   */
-
-   // Disable the attributes used by our shader
-
    safe_glDisableVertexAttribArray(h_aPosition);
 
    //Disable the shader
@@ -341,31 +235,19 @@ void ReshapeGL (int width, int height)
 
 void keyboard(unsigned char key, int x, int y ){
    switch( key ) {
-      /* M and N key move the object in x */
-   case 'd':
-      g_tx -= .1;
-      break;
-   case 'a':
-      g_tx += .1;
-      break;
-   case 'w':
-      g_ty += .1;
-      break;
-   case 's':
-      g_ty -= .1;
-      break;
-   case 'z':
-      g_tz -= .1;
-      break;
-   case 'x':
-      g_tz += .1;
-      break;
-   case 'q': case 'Q' :
-      exit( EXIT_SUCCESS );
+   case 'r':
+      // Reset all transformations
+      cube_translate = vec3(0.0);
+      cube_scale = 1.0;
+      cube_rotate = mat4(1.0); //The identity matrix
       break;
    }
    glutPostRedisplay();
 }
+
+bool mouse_left_down;
+bool mouse_right_down;
+int drag_begin_x, drag_begin_y;
 
 void mouse(int button, int state, int x, int y)
 {
@@ -373,14 +255,99 @@ void mouse(int button, int state, int x, int y)
    {
       if (state == GLUT_DOWN)
       {
-         printf("mouse clicked at %d %d\n", x, y);
+         printf("left mouse down at %d %d\n", x, y);
+         left_down = true;
+         drag_begin_x = x;
+         drag_begin_y = y;
+      }
+      else if (state == GLUT_UP)
+      {
+         printf("left mouse up at %d %d\n", x, y);
+         left_down = false;
+      }
+   }
+   else if (button == GLUT_RIGHT_BUTTON)
+   {
+      if (state == GLUT_DOWN)
+      {
+         printf("right mouse down at %d %d\n", x, y);
+         right_down = true;
+         drag_begin_x = x;
+         drag_begin_y = y;
+      }
+      else if (state == GLUT_UP)
+      {
+         printf("right mouse up at %d %d\n", x, y);
+         right_down = false;
       }
    }
 }
 
 void mouseMove(int x, int y)
 {
-   printf("mouse moved %d %d", x, y);
+   vec2 begin, end;
+   float beginDist, endDist;
+   if (mouse_left_down)
+   {
+      begin = screenToFractionalCoords(drag_begin_x, drag_begin_y);
+      end   = screenToFractionalCoords(x, y);
+      beginDist = sqrt(pow(begin.x, 2) + pow(begin.y, 2));
+      endDist   = sqrt(pow(x, 2) + pow(y, 2));
+   }
+
+   if (mouse_left_down && mouse_right_down)
+   {
+      // Scale
+
+      cube_scale *= beginDist / endDist;
+   }
+   else if (mouse_left_down)
+   {
+      // Rotate
+
+      float alpha;
+      vec3 u, v, axis;
+
+      // 1. compute 2 vec3's for mousedown and mouseup u and v
+      u = vec3(begin.x, begin.y, asin(1/beginDist));
+      v = vec3(end.x, end.y, asin(1/endDist));
+
+      // 2. compute axis of rotation u cross v
+      axis = vec3(
+         u.y*v.z - u.z*v.y,
+         u.z*v.x - u.x*v.z,
+         u.x*v.y - u.y*v.x);
+
+      // 3. compute angle of rotation alpha = acos(u dot v)
+      alpha = acos(u.x * v.x + u.y * v.y);
+      // 3a. convert rad to deg
+      alpha *= 180/atan(1)*4; //atan(1)*4 = pi
+
+      // 4. rotate(alpha, axis)
+      cube_rotate = glm::rotate(cube_rotate, alpha, axis);
+   }
+   else if (mouse_right_down)
+   {
+      // Pan
+      cube_translate = vec3(
+         cube_translate.x + x - drag_begin_x,
+         cube_translate.y + y - drag_begin_y,
+         cube_translate.z);
+   }
+
+   if (mouse_left_down || mouse_right_down)
+   {
+      //printf("mouse moved %d %d", x, y);
+      drag_begin_x = x;
+      drag_begin_y = y;
+   }
+}
+
+vec2 screenToFractionalCoords(int x, int y)
+{
+   return vec2(
+      2.0 * x/(float)glutGet(GLUT_WINDOW_WIDTH)  - 1,
+      2.0 * y/(float)glutGet(GLUT_WINDOW_HEIGHT) - 1);
 }
 
 int main( int argc, char *argv[] )
