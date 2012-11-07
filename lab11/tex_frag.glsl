e1ff106d7ea6e724b9f99996bf48197f8290b0a6
Mark Henry <mhenry02@calpoly.edu>
2012-11-06 17:08:28 -0800

    Colors be craaaaazy

--- lab11/tex_frag.glsl
+++ lab11/tex_frag.glsl
@@ -4,10 +4,17 @@ varying vec2 vTexCoord;
 varying vec3 vColor;
 
 void main(void) {
-  vec4 texColor0 = vec4(vColor.x, vColor.y, vColor.z, 1);
+  vec4 texColor0 = vec4(vColor, 1.0);
   vec4 texColor1 = texture2D(uTexUnit, vTexCoord);
 
-  gl_FragColor = vec4(vTexCoord.s, vTexCoord.t, 0, 1);
-  gl_FragColor = vec4(texColor1[0], texColor1[1], texColor1[2], 1);
+  float thresh = 0.5;
+  if (texColor1.b > thresh)
+  {
+     texColor1.g = texColor1.b;
+     texColor1.b = 0.0;
+  }
+
+  gl_FragColor = vec4(vTexCoord.s, vTexCoord.t, 0.0, 1.0);
+  gl_FragColor = vec4(texColor1.rgb, 1.0);
 }
 
